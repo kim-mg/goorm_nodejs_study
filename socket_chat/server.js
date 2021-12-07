0ca782295e75a5ecee415e36f426c471bbbff49c
kim-mg <>
2021-12-07 14:05:00 +0000

    socket_chat added real-time connnect function

--- socket_chat/server.js
+++ socket_chat/server.js
@@ -13,27 +13,48 @@ app.get('/', (req, res) => {
 	res.render('chat');
 });
 
+var users = {};
 var count = 1;
 // 채팅방에 접속했을 때 - 1
 io.on('connection', function(socket) {
 	console.log('user connected: ', socket.id);
-	var name = "익명" + count++;
+	var name = "익명" + count;
 	socket.name = name;
 	io.to(socket.id).emit('create name', name);
-	
+
 	io.emit('new_connect', name);
+	
+	// 접속중인 유저 확인
+	users[count] = {};
+	users[count].isconnect = 1;
+	users[count].id = socket.id;
+	users[count].name = socket.name;
+	count++;
+	console.log(users, count);
+	io.emit('update_users', users)
 	
 	// 채팅방 접속이 끊어졌을 때 -2
 	socket.on('disconnect', function() {
 		console.log('user disconnected: ' +
 				   socket.id + ' ' + socket.name);
+		for(var i=1; i<count; i++){
+			if(users[i].id == socket.id)
+				users[i].isconnect = 0;
+		};
+		io.emit('update_users', users)
 		io.emit('new_disconnect', socket.name);
 	});
 	
 	socket.on('send message', function(name, text) {
 		var msg = name + ' : ' + text;
-		if(name != socket.name)
+		if(name != socket.name){
+			for(var i=1; i<count; i++){
+				if(users[i].id == socket.id)
+					users[i].name = name;
+			};
+			io.emit('update_users', users);
 			io.emit('change name', socket.name, socket.name = name);
+		}
 		console.log(msg);
 		io.emit('receive message', msg);
 	});
