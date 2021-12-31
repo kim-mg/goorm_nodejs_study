3ed1ce0914c439b71803344ebddf4b0e4e9cd71d
kim-mg <>
2021-12-31 06:02:09 +0000

    12/31 case win and case live ingame user func added

--- socket_bingo/public/javascripts/main.js
+++ socket_bingo/public/javascripts/main.js
@@ -1,39 +1,59 @@
 var bingo = {
+	user_name: null,
 	is_my_turn: Boolean,
+	ongame: Boolean,
 	socket: null,
 	cur_table: {
 		rows: [0, 0, 0, 0, 0],
 		cols: [0, 0, 0, 0, 0],
 		diags: [0, 0]
 	},
+	numbers: [],
 	
 	init: function(socket){
 		var self = this;
 		var user_cnt = 0;
 		
 		this.is_my_turn = false;
-		// this.cur_table = {
-				
-		// };
+		this.ongame = false;
 		
 		socket = io();
 		
 		socket.on("check_number", function(data){
-			self.where_is_it(data.num);
-			self.print_msg(data.username + "님이 '" + data.num + "'을 선택했습니다.");
+			self.where_is_it(data.num, socket);
+			self.print_msg(data.username + "님이 '" + data.num + "'번을 선택했습니다.");
 			// self.check_bingo();
 		});
 		
 		socket.on("game_started", function(data){
 			console.log("enter the game_started");
+			self.ongame = true;
 			self.print_msg(data + " 님이 게임을 시작했습니다.");
 			$("#start_button").hide();
+			$("#restart_button").show();
 		});
 		
 		socket.on("update_users", function(data, user_count){
 			console.log(data, user_count, user_cnt);
 			user_cnt = user_count;
 			self.update_userlist(data, socket);
+		});
+		
+		socket.on("game_ended", function(user, flag){
+			console.log("ended game");
+			self.ongame = false;
+			if (flag == 1){
+				self.print_msg("<알림> " + user + "님이 승리하셨습니다.");
+			}
+			else if (flag == 2){
+				self.print_msg(user + "님이 게임을 종료했습니다.");
+				self.print_msg("<알림> 게임이 종료되었습니다.");
+			}
+		});
+		
+		socket.on("game_restarted", function(){
+			self.regame();
+			self.print_msg("<알림> 게임을 다시 시작합니다.");
 		});
 		
 		// 도메인 주소로부터 연결되었을 경우 form으로부터 받은 정보
@@ -44,17 +64,12 @@ var bingo = {
 		});
 		*/
 		
-		var numbers = [];
+		var numbers = self.numbers;
 		for(var i=1; i<=25; i++){
 			numbers.push(i);
 		}
 		
-		numbers.sort(function(a,b){
-			var temp = parseInt(Math.random() * 10);
-			var isOddOrEven = temp%2;
-			var isPosOrNeg = temp > 5 ? 1 : -1;
-			return (isOddOrEven*isPosOrNeg);
-		});
+		self.sorted_num();
 		
 		$("#user_info").on("submit", function(e){
 			socket.emit("rename", $("#name").val());
@@ -64,12 +79,16 @@ var bingo = {
 		
 		$("table.bingo-board td").each(function(i){
 			$(this).html(numbers[i]);
-			
+		
 			$(this).click(function(){
 				if(user_cnt == 1){
 					self.print_msg("<알림> 최소 2명부터 게임이 가능합니다.");
 				}
+				else if(self.ongame == false){
+					self.print_msg("<알림> 게임을 시작하십시오.");
+				}
 				else{
+					self.print_msg("'" + numbers[i] + "'번을 선택하셨습니다.");
 					self.select_num(this, socket, i);
 				}
 			});
@@ -82,10 +101,39 @@ var bingo = {
 			else{
 				socket.emit('game_start');
 				self.print_msg("<알림> 게임을 시작합니다.");
+				self.ongame = true;
 				$("#start_button").hide();
+				$("#restart_button").show();
 			}
 		});
 		
+		$("#restart_button").click(function(){
+			socket.emit("restart_game");
+			
+		});
+	},
+	
+	regame: function(){
+		var self = this;
+		console.log("restart game!!");
+		
+		self.sorted_num();
+		
+		$("table.bingo-board td").each(function(i){
+			$(this).html(self.numbers[i]);
+			self.restart_num(this);
+		});
+	},
+	
+	sorted_num: function(){
+		var self = this;
+		
+		self.numbers.sort(function(a,b){
+			var temp = parseInt(Math.random() * 10);
+			var isOddOrEven = temp%2;
+			var isPosOrNeg = temp > 5 ? 1 : -1;
+			return (isOddOrEven*isPosOrNeg);
+		});
 	},
 	
 	// init 끝
@@ -97,7 +145,7 @@ var bingo = {
 			socket.emit("select", { num: $(obj).text() });
 			self.count_bingo(idx);
 			self.check_num(obj);
-			self.check_bingo(idx);
+			self.check_bingo(idx, socket);
 			self.is_my_turn = false;
 		}
 		else{
@@ -105,15 +153,15 @@ var bingo = {
 		}
 	},
 	
-	where_is_it: function(num){
+	where_is_it: function(num, socket){
 		var self =  this;
 		var obj = null;
 		
 		$("table.bingo-board td").each(function(i){
 			if($(this).text() == num) {
 				self.count_bingo(i);
-				self.check_bingo(i);
 				self.check_num(this);
+				self.check_bingo(i, socket);
 			}
 		});
 	},
@@ -128,21 +176,25 @@ var bingo = {
 
 		if (diagonal_left == 0)
 			self.cur_table.diags[0]++;
-		if (diagonal_right == 0)
+		if (idx < 21 && diagonal_right == 0)
 			self.cur_table.diags[1]++;
 		self.cur_table.rows[quotient]++;
 		self.cur_table.cols[remainder]++;
 
-		console.log(self.cur_table);
+		console.log(self.cur_table, idx, diagonal_left, diagonal_right);
 	},
 	
-	check_bingo: function(idx){
+	check_bingo: function(idx, socket){
+		var self = this;
 		var q = parseInt(idx / 5);
 		var r = idx % 5;
-		if (cur_table[q] == 5 || cur_table[r] == 5 || cur_table[0] == 5 || cur_table[1] == 5){
-			// user가 승리하였습니다.
-			
-			// 게임 init 다시~
+		console.log("check_bingo : ", q, r, self.cur_table, self.cur_table[q], self.cur_table[r]);
+		if (self.cur_table.rows[q] == 5 || self.cur_table.cols[r] == 5 || self.cur_table.diags[0] == 5 || self.cur_table.diags[1] == 5){
+			// user가 승리하였습니다. server로 보내서 broadcast로 전체 출력 후 게임 진행 불가
+			socket.emit("game_end", self.user_name);
+			self.print_msg(self.user_name + "님이 승리하셨습니다.");
+			self.ongame = false;
+			// 게임 init 다시~ : 태그에 있던 것들 초기화 빙고판 초기화
 		}
 	},
 	
@@ -150,6 +202,12 @@ var bingo = {
 		$(obj).css("text-decoration", "line-throuhg");
 		$(obj).css("color", "lightgray");
 		$(obj).attr("checked", true);
+	},
+	
+	restart_num: function(obj){
+		$(obj).css("text-decoration", "");
+		$(obj).css("color", "");
+		$(obj).removeAttr("checked");
 	},
 	
 	update_userlist: function(data, this_socket){
@@ -169,6 +227,7 @@ var bingo = {
 					}
 				}
 				if(value.id == this_socket.id){
+					self.user_name = value.username;
 					$("#list").append("<font color='DodgerBlue'>" +
 									 turn + value.username + 
 									 "<br></font>");
@@ -186,6 +245,7 @@ var bingo = {
 		$("#logs").append(msg + "<br />");
 		$('#logs').scrollTop($('#logs')[0].scrollHeight);
 	}
+	
 };
 
 $(document).ready(function(){
--- socket_bingo/server.js
+++ socket_bingo/server.js
@@ -5,13 +5,12 @@ var io = require('socket.io')(http);
 var path = require('path');
 
 // view engine setup
-app.set('views', path.join(__dirname, '/public/views'));
+app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-	res.render('main', { title: '온라인 빙고 게임', 
-					   username: req.query.username });
+	res.render('main', { title: '온라인 빙고 게임', username: req.query.username });
 });
 
 var users = {};
@@ -19,6 +18,7 @@ var user_count = 0;
 var user_nums = 0;
 var start_idx = 0;
 var turn_count = 0;
+var ingame_user_cnt = 0;
 
 io.on('connection', function(socket){
 	console.log('user connected : ' + socket.id);
@@ -54,12 +54,12 @@ io.on('connection', function(socket){
 	});
 	*/
 	
-	socket.on('rename', function(username){
-		if(username != socket.name){
+	socket.on('rename', function(re_username){
+		if(re_username != socket.username){
 			for(var i=start_idx; i<user_nums; i++){
 				if(users[i].id == socket.id)
-					users[i].username = username;
-					socket.username = username;
+					users[i].username = re_username;
+					socket.username = re_username;
 			}
 			io.emit('update_users', users, user_count);
 		}
@@ -67,10 +67,19 @@ io.on('connection', function(socket){
 	
 	socket.on('game_start', function(){
 		socket.broadcast.emit("game_started", socket.username);
+		ingame_user_cnt = user_count;
 		turn_count = start_idx;
 		users[turn_count].turn = true;
 		
 		io.emit('update_users', users);
+	});
+	
+	socket.on('game_end', function(winner){
+		socket.broadcast.emit("game_ended", winner, 1);
+	});
+	
+	socket.on('restart_game', function(){
+		io.emit("game_restarted");
 	});
 	
 	socket.on('select', function(data){
@@ -93,20 +102,29 @@ io.on('connection', function(socket){
 		
 		users[turn_count].turn = true;
 		
-		io.sockets.emit('update_users', users);
+		io.emit('update_users', users);
 	});
 	
 	socket.on('disconnect', function(){
 		console.log('user disconnected : ' + socket.id, socket.username);
-		for(var i=start_idx; i<user_count; i++){
+		for(var i=start_idx; i<user_nums; i++){
 			if(users[i].isconnect == 1 && users[i].id == socket.id){
 				users[i].isconnect = 0;
-				if(i == start_idx)
-					start_idx++;
+				if(i == start_idx){
+					for(var j=start_idx; j<user_nums; j++){
+						if(users[j].isconnect == 1){
+							start_idx = j;
+							break;
+						}
+					}
+				}
 			}
 		};
 		
 		user_count--;
+		if (user_count < ingame_user_cnt){
+			io.emit('game_ended', socket.username, 2);
+		}
 		io.emit('update_users', users, user_count);
 	});
 });
